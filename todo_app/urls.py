4da6a0cb70e90b346010e8fe3ad555cfa6861a9f
Utkarsh Tekwani <109906532+utkarshtekwani@users.noreply.github.com>
2025-11-29 15:11:03 +0530

    Update urls.py

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    
     path("", views.ListListView.as_view(), name="index"),
     path("list/<int:list_id>/",views.ItemListView.as_view(),name ="list"),
     path("list/add/",views.ListCreate.as_view(), name = "list-add"),
